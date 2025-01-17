01b4beb3930feaaf7f188677eb4993eb4152bfc6
taka <takayuki@examlpe.com>
2025-01-17 15:38:35 +0900

    KeijibanDao作成

--- src/main/java/dao/KeijibanDao.java
+++ src/main/java/dao/KeijibanDao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.List;
+
+import domain.Keijiban;
+
 public interface KeijibanDao {
+	
+	List<Keijiban> findAll() throws Exception;
+	
+	void insert(Keijiban keijiban) throws Exception;
+	
+	void delete(int id) throws Exception;
 
 }
